9daf0b4e17d302f89d05c3e95097ffde0343c959
Mario A Chavez <mario.chavez@gmail.com>
2013-01-30 16:55:58 -0600

    Project bootstrap

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Assets should be precompiled for production (so we don't need the gems loaded then)
+Bundler.require(*Rails.groups(assets: %w(development test)))
+
+module Community
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Custom directories with classes and modules you want to be autoloadable.
+    # config.autoload_paths += %W(#{config.root}/extras)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Community::Application.config.secret_key_base = 'aa4499f74569d7cfc9608471298e7d4a8cb7860bdb831fea2db67865aee9a095a8e86ac6058fe7eb588a23fe51495ca76d33eeca4359618deb4a9129e703f9bb'
