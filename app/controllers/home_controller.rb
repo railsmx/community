c38e88b6fb92b6291fd4181ab93e7cde5fdc62a4
Mario A Chavez <mario.chavez@gmail.com>
2013-02-04 16:33:49 -0600

    Remove contacto from show action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,15 @@
 class HomeController < ApplicationController
 
-  
+
   #@contact_form.request = request
 
   def index
-  
+
   end
 
   def show
-    
-    pages = %w(acerca contacto privacidad)
+    pages = %w(acerca privacidad)
     page = params[:id] || ''
-    @contact_form = ContactForm.new(params[:contact_form])
     return render page.downcase if pages.include?(page.downcase)
 
     render text: 'Not found', status: 404
