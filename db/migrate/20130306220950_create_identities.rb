e8893d9fc89961a043797de7277f47bb67961483
Mario A Chavez <mario.chavez@gmail.com>
2013-03-12 16:36:50 -0600

    A direct change on old users migration to become identities migration

--- db/migrate/20130306220950_create_identities.rb
+++ db/migrate/20130306220950_create_identities.rb
@@ -1,12 +1,11 @@
-class AddColumsToIdentities < ActiveRecord::Migration
+class CreateIdentities < ActiveRecord::Migration
   def change
-    drop_table :identities
     create_table :identities do |t|
       t.string :provider, :null => false
       t.string :uid, :null => false
       t.string :username, :null => false
       t.string :email, :null => false
-      t.boolean :block, :default => false 
+      t.boolean :block, :default => false
 
       t.timestamps
     end
--- db/migrate/20130312190959_change_name_from_users_to_identities.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNameFromUsersToIdentities < ActiveRecord::Migration
-  def change
-    rename_table :users, :identities
-  end
-end
