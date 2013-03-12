cf6c7d19720321de47f9024c194434ff291627f7
Mario A Chavez <mario.chavez@gmail.com>
2013-03-12 12:34:02 -0600

    Add login_user_test for integration (WIP)

---
+++ test/integration/login_user_test.rb
@@ -0,0 +1,13 @@
+require 'minitest_helper'
+
+feature 'Login User Test' do
+
+  scenario 'Not logged user' do
+  end
+
+  scenario 'Login user' do
+  end
+
+  scenario 'Reject blocked user' do
+  end
+end
