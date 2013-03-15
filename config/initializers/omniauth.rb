2787dace6e3c5a840d45ae5913ad7d744e9fdb78
Mario A Chavez <mario.chavez@gmail.com>
2013-03-15 13:51:20 -0600

    Fix an invalid tag Remove keys from initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '63e748f8fe4931608503', '308f12f4d1c3bc18cfdbf11d047e525ca76ec5bd'
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
