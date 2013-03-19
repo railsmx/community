67c382de19922ac6463797ae14c739ceeb255552
Mario A Chavez <mario.chavez@gmail.com>
2013-03-19 10:19:18 -0600

    Fix a typo

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,22 +1,22 @@
 Rails.application.config.middleware.use Warden::Manager do |manager|
   manager.default_strategies :omniauth_public
-  manager.failure_app = lambda { |env| DashboardController.actions(:index).call(env) }
+  manager.failure_app = lambda { |env| DashboardController.action(:index).call(env) }
 end
- 
+
 Warden::Manager.serialize_into_session(:identity) do |identity|
   identity.id
 end
- 
+
 Warden::Manager.serialize_from_session(:identity) do |id|
   Identity.find(id)
 end
- 
+
 Warden::Strategies.add(:omniauth_public) do
   def valid?
     controller_name = request.env['action_dispatch.request.parameters']['controller']
     controller_name == 'sessions'
   end
- 
+
   def authenticate!
     auth = request.env['omniauth.auth']
     if valid?
