6f9bfcdb77ee15332cdd510e7ad4bb81e7eb7c2f
Mario A Chavez <mario.chavez@gmail.com>
2013-03-21 15:15:50 -0600

    Do not store keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, 'f8b0410a919f68884625', '889c996d173e39fd8134c0f9dbd4160eee0a8183'
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
