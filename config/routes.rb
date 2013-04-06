8bb0dc660f002ea69ce3a5522b54d23c68a2e03f
memoxmrdl <jmemox@gmail.com>
2013-04-05 23:14:29 -0600

    refactor just add :path => /eventos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Community::Application.routes.draw do
-  #resources :events
+  resources :events, :path => '/eventos'
 
   get "/auth/:provider/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout
@@ -9,17 +9,6 @@ Community::Application.routes.draw do
 
   get "/privacidad" => "home#privacy"
   get "/acerca" => "home#about"
-
-  get "/eventos" => "events#index", as: :events
-  get "/eventos/:id" => "events#show", as: :event
-  get "/eventos/new" => "events#new", as: :new_event
-  get "/eventos/:id/edit" => "events#edit", as: :edit_event
-  post "/eventos" => "events#create"
-  patch "/eventos/:id" => "events#update"
-  put "/eventos/:id" => "events#update"
-  delete "/eventos/:id" => "events#destroy"
-
-  get "/:id" => "home#show", as: :static
 
   if Rails.env.test?
     post '/sessions/create' => 'sessions#create'
