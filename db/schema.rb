7bcd7419c6ea5dccfea9eb89a0be24c19fff46db
Angel Solorio & Jose Guillermo <pair+angel.solorio+guillermo.moreno@crowdint.com>
2013-04-08 10:01:50 -0500

    working in progress views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130407064420) do
+ActiveRecord::Schema.define(version: 20130406092758) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "events", force: true do |t|
     t.string   "name",        null: false
