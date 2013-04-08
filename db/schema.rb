42075829f8cc3a9df3b074c707ac9cc11edb4989
Mario A Chavez <mario.chavez@gmail.com>
2013-04-08 11:20:59 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130406092758) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "events", force: true do |t|
     t.string   "name",        null: false
     t.text     "description", null: false
