e5e1c33c38aa57770412a1073a16ded9b8225a73
Angel Solorio & Jose Guillermo <pair+angel.solorio+guillermo.moreno@crowdint.com>
2013-04-15 15:55:35 -0500

    Line added about the ENV[THEME] variable to pass the tests

--- config/initializers/community.rb
+++ config/initializers/community.rb
@@ -1,2 +1,4 @@
+ENV['THEME'] = 'railsmx' if Rails.env.test?
+
 Rails.application.config.theme = ENV['THEME'].downcase
 Rails.application.config.assets.precompile += ["#{ENV['THEME'].downcase}.css", "#{ENV['THEME'].downcase}.js"]
