25f0602b4f6d5916aa6960efee2f35401a03485e
Mario A Chavez <mario.chavez@gmail.com>
2013-04-17 17:09:19 -0500

    Remove initializers memcached Setup memcached in production

--- config/initializers/memcached.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.cache_store = :dalli_store,
-  ENV["MEMCACHIER_SERVERS"].split(","),
-  { username: ENV["MEMCACHIER_USERNAME"],
-    password: ENV["MEMCACHIER_PASSWORD"] } if Rails.env.production?
