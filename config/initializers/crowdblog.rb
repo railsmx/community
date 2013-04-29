05c93e3280eb25cef2e73304680d5d4bebf35c6c
Angel Solorio & Jose Guillermo <pair+angel.solorio+guillermo.moreno@crowdint.com>
2013-04-29 18:01:57 -0500

    add other owner published

--- config/initializers/crowdblog.rb
+++ config/initializers/crowdblog.rb
@@ -1,7 +1,7 @@
 Crowdblog.user_class = 'Identity'
 
 Rails.application.config.discus_key = '434324234'
-Rails.application.config.publishers = 'guillermo.moreno@crowdint.com'
+Rails.application.config.publishers = 'guillermo.moreno@crowdint.com, angel.solorio@crowdint.com'
 
 Crowdblog::PostsController.class_eval do
   layout 'application'
