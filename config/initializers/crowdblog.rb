07667601b69a355187e21ff6deb90ed873c532f7
Angel Solorio & Giovanni Mendoza <pair+angel.solorio+giovanni.mendoza@crowdint.com>
2013-05-06 14:05:50 -0500

    lines added to support test

--- config/initializers/crowdblog.rb
+++ config/initializers/crowdblog.rb
@@ -1,4 +1,9 @@
 Crowdblog.user_class = 'Identity'
 
 Rails.application.config.discus_key = '434324234'
-Rails.application.config.publishers = 'guillermo.moreno@crowdint.com, angel.solorio@crowdint.com, mendozagioo@gmail.com'
+Rails.application.config.publishers = 'guillermo.moreno@crowdint.com, angel.solorio@crowdint.com, jmemox@gmail.com'
+
+
+if Rails.env.test?
+	Rails.application.config.publishers = 'test@user.com'
+end
