35337fda2878641ecf58a46d8760f63855285172
Angel Solorio & Jose Guillermo <pair+angel.solorio+guillermo.moreno@crowdint.com>
2013-05-07 10:28:31 -0500

    test integration about access admin section blog

--- test/integration/blog_management_test.rb
+++ test/integration/blog_management_test.rb
@@ -2,18 +2,21 @@ require 'test_helper'
 
 feature 'BlogManagement Feature Test' do
   background do
-    mock_omniauth
+    mock_omniauth_blog(12334, 'test@user.com')
   end
 
   scenario "when current user is publisher" do
+    login_user
 
     visit "/blog/admin"
 
-    save_and_open_page
-    page.must_have_selector('a.brand', text: 'Crowdblog')
+    page.must_have_selector('div.span12', text: 'Posts')
   end
 
   scenario "when current user isn't publisher" do
+    mock_omniauth_blog(12263, 'test2@user.com')
+
+    login_user
 
     visit "/blog/admin"
 
@@ -21,3 +24,17 @@ feature 'BlogManagement Feature Test' do
   end
 end
 
+def login_user
+  visit '/'
+
+  find('.sign-in').click
+end
+
+def mock_omniauth_blog(uid = '12334', email)
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+    { 'provider' => 'github', 'uid' => uid,
+      'info' => { 'nickname' => 'Test user', 'email' => email }
+    }
+  )
+end
