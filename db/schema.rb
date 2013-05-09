7fab6dd4ff759d8b14109a8aa7990290a4fac0c8
Angel Solorio <angel.solorio@crowdint.com>
2013-05-09 12:37:46 -0500

    Migration ran

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130507142525) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "crowdblog_assets", force: true do |t|
     t.integer  "post_id"
     t.string   "attachment"
