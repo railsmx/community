c1e8cb291911fed4a8488fb5eeb2b87b691999c2
Mario A Chavez <mario.chavez@gmail.com>
2013-05-10 10:20:06 -0500

    create_crowdblog_users.crowdblog.rb is not need it anymore

--- db/migrate/20130423181246_create_crowdblog_users.crowdblog.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from crowdblog (originally 20121016063750)
-class CreateCrowdblogUsers < ActiveRecord::Migration
-  def change
-    create_table :crowdblog_users do |t|
-      t.string :email
-      t.string :name
-    end
-  end
-end
