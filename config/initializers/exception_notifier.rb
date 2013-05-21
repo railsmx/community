231bbc2417ee9831a779fb0de40e9951095c71f3
Jose Guillermo & Rafael Ramos <pair+guillermo.moreno+rafael.ramos@crowdint.com>
2013-05-21 18:20:29 -0500

    config notifier email to exception_notifier

---
+++ config/initializers/exception_notifier.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.use ExceptionNotifier,
+:email => {
+  :email_prefix => "#{ENV['THEME'].downcase} Exception Notifier ",
+  :sender_address => %{"notifier" <#{ENV['EMAIL_NOTIFIER'].split(',')}>},
+  :exception_recipients => ENV['EMAIL_NOTIFIER'].split(',')
+}
